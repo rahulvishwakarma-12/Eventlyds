f071a1a262b8483952e26295f4ed71d80e7f7768
Rahul Vishwakarma <rahul@kaetoo.in>
2024-12-20 16:32:29 +0530

    final V1

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 
 export async function POST(req: Request) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
@@ -50,19 +52,42 @@ export async function POST(req: Request) {
   const { id } = evt.data
   const eventType = evt.type
 
-  if(eventType == 'user.created'){
-    const{id,username,email_addresses,first_name,last_name,image_url} = evt.data;
-    console.log("user create successful")
+  if(eventType === "user.created"){
+    const{id,email_addresses, image_url,first_name,last_name, username } = 
+    evt.data;
+    await createUser({
+      clerkId:id,
+      username:username || "MyUser",
+      email:email_addresses[0].email_address,
+      photo:image_url,
+      firstName:first_name || "",
+      lastName:last_name || ""
+    });
+    NextResponse.json("User Created");
   }
-  if(eventType == 'user.updated'){
-    const{username,first_name,last_name} = evt.data;
-    console.log("user Update successful")
 
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
+
+    const user = {
+      firstName: first_name || "",
+      lastName: last_name || "",
+      username: username || "MyUser",
+      photo: image_url,
+    }
+
+    const updatedUser = await updateUser(id, user)
+
+    return NextResponse.json({ message: 'OK', user: updatedUser })
   }
-  if(eventType == 'user.deleted')[
-    console.log("user delete successful")
 
-  ]
+  if (eventType === 'user.deleted') {
+    const { id } = evt.data
+
+    const deletedUser = await deleteUser(id!)
+
+    return NextResponse.json({ message: 'OK', user: deletedUser })
+  }
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
 
