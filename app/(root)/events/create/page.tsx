c808222f61907ca98ad4cdb1d28bd72f0946ec5c
rahulvishwakarma-12 <contact@rahulvishwakarma.net>
2024-12-20 19:43:02 +0530

    Update page.tsx

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 
 const CreateEvent = () => {
   
-  const userId = "67654f460bc6a12dc01b7774";
+  const userId = "67657a94b7a7a7b2c42c53c2";
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
