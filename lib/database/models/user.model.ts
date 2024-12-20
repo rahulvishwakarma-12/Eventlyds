a29a0c3d5351f154cf8091fc33adbfa2fa854507
Rahul Vishwakarma <rahul@kaetoo.in>
2024-12-20 15:47:38 +0530

    Testing

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,14 +1,34 @@
-import { Schema, model, models } from "mongoose";
+// lib/models/user.model.ts
 
-const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
-  photo: { type: String, required: true },
-})
+import mongoose from 'mongoose';
 
-const User = models.User || model('User', UserSchema);
+const userSchema = new mongoose.Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  photo: {
+    type: String,
+  },
+});
 
-export default User;
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export { User };
